95ce9e8fe35697bf4ad504818754c585901809d7
artemegoroff <artem_egoroff@ro.ru>
2018-09-29 01:06:22 +0300

    28 september

--- theory/admin.py
+++ theory/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import CategoryTheory,TheoryVideo
 
 
+
+class AdminTheoryVideo(admin.ModelAdmin):
+    list_display = ["name", 'category']
+
+
+
+
 admin.site.register(CategoryTheory)
-admin.site.register(TheoryVideo)
+admin.site.register(TheoryVideo,AdminTheoryVideo)
