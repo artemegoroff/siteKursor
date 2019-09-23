bb82651af4b568d8dceb64d016a6ec58bea8a785
artemegoroff <artem_egoroff@ro.ru>
2019-09-23 22:35:39 +0300

    завез сброс пароля

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,4 +8,9 @@ urlpatterns = [
     url('login/', views.login_user, name='login'),
     url('logout/', views.logout_user, name='logout'),
     url('profile/', views.get_profile_page, name='profile'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
