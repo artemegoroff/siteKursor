b2ead0bf4c2f044ed799e5b9203ef92d32ff610a
artemegoroff <artem_egoroff@ro.ru>
2019-09-24 21:51:45 +0300

    завез сброс пароля

--- Kursor/email_info.py
+++ Kursor/email_info.py
@@ -3,4 +3,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'egoroff.channel@gmail.com'
-EMAIL_HOST_PASSWORD = 'prosto123!@#'
+EMAIL_HOST_PASSWORD = 'FUfdsfdREGDahfds321d@1'
