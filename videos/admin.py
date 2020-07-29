5fc4c5c8d84e3ba633503938da5b055a704c9d21
artemegoroff <artem_egoroff@ro.ru>
2020-07-30 01:07:59 +0300

    boosty админ фильтр

--- videos/admin.py
+++ videos/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Course,ProgrammTask,InputOutputData
-# Register your models here.
+
+
+class HaveDicision(admin.SimpleListFilter):
+
+    title = 'have_dicision'
+    parameter_name = 'decision'
+
+    def lookups(self, request, model_admin):
+
+        return (
+            ('True', 'Нет решений'),
+            ('False', 'Есть решения'),
+        )
+
+    def queryset(self, request, queryset):
+        kwargs = {
+            '%s' % self.parameter_name : None,
+        }
+        if self.value() == 'True':
+            return queryset.filter(**kwargs)
+        if self.value() == 'False':
+            return queryset.exclude(**kwargs)
+        return queryset
+
+
 
 class AdminCourseSummernote(SummernoteModelAdmin):
     summernote_fields = ('article',)
@@ -12,8 +36,11 @@ class AdminCourseSummernote(SummernoteModelAdmin):
     prepopulated_fields = {"slug": ("theme",)}
     list_editable = ['number_theme','boosty','patreon','is_closed_video']
 
+
+
 class AdminProgrammTask(admin.ModelAdmin):
     filter_horizontal = ['examples']
+    list_filter = (HaveDicision,)
     search_fields = ('name',)
     list_display = ['name', 'url_ref','decision', 'boosty', 'patreon']
     list_editable = ['url_ref','decision', 'boosty', 'patreon']
