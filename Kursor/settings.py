da62ee59ed8cba94f93f2a8960fd8a809082b491
artem_egorov <prosto123!@#>
2026-04-18 02:03:08 +0300

    .env

--- Kursor/settings.py
+++ Kursor/settings.py
@@ -1,23 +1,34 @@
 import os
-from .email_info import *
+from dotenv import load_dotenv
+from pathlib import Path
 
-EMAIL_BACKEND = EMAIL_BACKEND
-EMAIL_HOST = EMAIL_HOST
-EMAIL_USE_TLS = EMAIL_USE_TLS
-EMAIL_PORT = EMAIL_PORT
-EMAIL_HOST_USER = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '857jw)9!hrhnt7=61z@(@@=08*w4^*s!xpjekpaf#j*jrlff%i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'egoroffartem.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'egoroffpython.ru',
+    'www.egoroffpython.ru',
+]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
@@ -37,7 +48,6 @@ INSTALLED_APPS = [
     'theory',
     'robots',
     'django_summernote',
-    # 'accounts',
     'services',
     'sponsorship'
 ]
@@ -122,9 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    ('base_static', os.path.join(BASE_DIR, "base_static")),
+    os.path.join(BASE_DIR, 'base_static'),
 )
 
 # Авторизация
