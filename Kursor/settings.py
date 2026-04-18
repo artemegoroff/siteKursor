9016b163ba1dcb85c42519afc672b6ea5055a760
artem_egorov <prosto123!@#>
2026-04-18 18:26:31 +0300

    legacy

--- Kursor/settings.py
+++ Kursor/settings.py
@@ -99,8 +99,7 @@ DATABASES = {
     },
     "legacy": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "old_db.sqlite3",
-    }
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")    }
 }
 
 # Password validation
